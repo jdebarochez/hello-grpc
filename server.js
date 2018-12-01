32bb0297fe1a125578f10be07b89abb8ebfb5444
Jean de BAROCHEZ <jdebarochez@amaris.com>
2018-12-01 20:30:49 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var PROTO_PATH = __dirname + '/helloworld.proto';
+
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+
+var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+var helloworld = protoDescriptor.helloworld;
+
+function doSayHello(call, callback) {
+  console.log('Saying hello!');
+  callback(null, {
+    message: 'Hello! ' + call.request.name
+  });
+}
+
+function getServer() {
+  var server = new grpc.Server();
+  server.addService(helloworld.Greeter.service, {
+    sayHello: doSayHello
+  });
+  return server;
+}
+
+if (require.main === module) {
+  var server = getServer();
+  server.bind('0.0.0.0:9090', grpc.ServerCredentials.createInsecure());
+  console.log('listening at 9090');
+  server.start();
+}
+
+exports.getServer = getServer;
